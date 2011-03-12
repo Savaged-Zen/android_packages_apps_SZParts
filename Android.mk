63b33d8cd23b8bcefbb76957f82ec0549b9b4f8f
root <root@josh.savaged-zen.org>
2011-03-12 16:39:32 +0000

    first mod

--- Android.mk
+++ Android.mk
@@ -1,11 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := SZParts
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
